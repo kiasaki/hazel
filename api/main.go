27a6025c879da5668f170d9ea4389aec65fadc2b
Frederic Gingras <frederic@gingras.cc>
2015-03-10 22:17:07 -0400

    Basic api working

--- api/main.go
+++ api/main.go
@@ -1,41 +1,90 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net"
 	"net/http"
+	"os"
+	"time"
 
+	"github.com/goamz/goamz/aws"
+	"github.com/goamz/goamz/s3"
 	"github.com/jessevdk/go-flags"
-	"github.com/maxwellhealth/bongo"
 	"github.com/zenazn/goji"
+	"github.com/zenazn/goji/graceful"
+	"github.com/zenazn/goji/web"
 )
 
+type jsonMap map[string]interface{}
+
 var cfg struct {
-	Name string `short:"n" long:"name" description:"A name" required:"true"`
+	Port         string `short:"p" long:"port" env:"PORT" description:"Port to bind to" default:"8000"`
+	AwsAccessKey string `long:"aws-access-key" env:"AWS_ACCESS_KEY" description:"Aws access key" required:"true"`
+	AwsSecretKey string `long:"aws-secret-key" env:"AWS_SECRET_KEY" description:"Aws secret key" required:"true"`
 }
 
-var connection bongo.Connection
+var awsAuth *aws.Auth
+var awsS3 *s3.S3
 
 func main() {
 	initConfig()
-	initDb()
+	initAws()
+	initServer()
 }
 
 func initConfig() {
 	_, err := flags.Parse(&cfg)
 	if err != nil {
-		panic(err)
+		os.Exit(1)
 	}
 }
 
-func initDb() {
-	var err error
+func initAws() {
+	awsAuth = aws.NewAuth(cfg.AwsAccessKey, cfg.AwsSecretKey, "", time.Time{})
+	awsS3 = s3.New(*awsAuth, aws.USEast)
+}
 
-	config := &bongo.Config{
-		ConnectionString: "localhost",
-		Database:         "bongotest",
-	}
+func initServer() {
+	goji.Get("/", handleIndex)
 
-	connection, err = bongo.Connect(config)
+	listener, err := net.Listen("tcp", ":"+cfg.Port)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
+
+	goji.DefaultMux.Compile()
+
+	http.Handle("/", goji.DefaultMux)
+
+	log.Println("Starting Goji on", listener.Addr())
+	graceful.HandleSignals()
+	graceful.PreHook(func() { log.Printf("Goji received signal, gracefully stopping") })
+	graceful.PostHook(func() { log.Printf("Goji stopped") })
+
+	err = graceful.Serve(listener, http.DefaultServeMux)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	graceful.Wait()
+}
+
+func sendJson(w http.ResponseWriter, entity interface{}, status int) {
+	b, err := json.Marshal(entity)
+	if err != nil {
+		status = http.StatusInternalServerError
+		b = []byte(fmt.Sprintf(`{error:"%s"}`, err.Error()))
+	}
+
+	body := string(b[:])
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	fmt.Fprint(w, body)
+}
+
+func handleIndex(c web.C, w http.ResponseWriter, r *http.Request) {
+	sendJson(w, jsonMap{"index": true}, http.StatusOK)
 }
